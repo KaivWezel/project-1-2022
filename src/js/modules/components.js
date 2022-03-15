f2c2c9851a5e6a979f621f4f4a737d1696b02ac2
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-15 12:45:52 +0100

    Create components.js

---
+++ src/js/modules/components.js
@@ -0,0 +1,11 @@
+const eventCard = (event) => {
+	// Create elements in document
+	const card = document.createElement("article");
+	const hero = document.createElement("img");
+	const title = document.createElement("h4");
+	const desc = document.createElement("p");
+	// Assign content to elements
+	// Append elements to card
+};
+
+let bookCard;
