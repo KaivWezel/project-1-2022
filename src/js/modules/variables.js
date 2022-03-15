8039201484eb87a3814cbb0d781150bda6160f68
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-15 12:45:49 +0100

    Update variables.js

--- src/js/modules/variables.js
+++ src/js/modules/variables.js
@@ -1,5 +1,6 @@
-const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
-const key = "dc0e2f073c03758140452044906bc818";
+// const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
+const endpoint = "http://localhost:3000/results";
+const key = "0076bc3bc11d080e07a303360178002a";
 const secret = "6b02878111c06660300798cf4c40a685";
 
 export { endpoint, key, secret };
