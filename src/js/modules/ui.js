6070ea60644c5063ef2f374418cdcf930612da45
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-16 15:36:07 +0100

    added local db for testing purposes

--- src/js/modules/ui.js
+++ src/js/modules/ui.js
@@ -1,10 +1,11 @@
 import getData from "./fetch.js";
 import { createCard } from "./components/Card.js";
 import Render from "./Renderer.js";
+import { localDb } from "./variables.js";
 
 export const loadBooks = async () => {
 	const bookshelf = document.querySelector(".bookshelf");
-	const books = await getData();
+	const books = await getData(localDb);
 	books.forEach((book) => {
 		const card = createCard(book);
 		Render(card, bookshelf);
--- src/js/modules/variables.js
+++ src/js/modules/variables.js
@@ -1,6 +1,7 @@
 // const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
+
 const endpoint = "http://localhost:3000/results";
 const key = "0076bc3bc11d080e07a303360178002a";
 const secret = "6b02878111c06660300798cf4c40a685";
-
-export { endpoint, key, secret };
+const localDb = "http://localhost:3000/results";
+export { endpoint, key, secret, localDb };
