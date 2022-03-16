e66ee3a45c3dc69e896e8b0eb9210fbdba431037
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-16 11:40:34 +0100

    Removed loose code

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,7 @@
 // const url = "https://zoeken.oba.nl/api/v1/";
-import getData from "./modules/fetch.js";
+// import fetch from "./modules/fetch.js";
+import addEventListeners from "./modules/eventListeners.js";
+
+addEventListeners();
 
 console.log("hello");
-
-const logger = async () => {
-	const data = await getData();
-	console.log("data", data);
-};
-
-logger();
-
-// const endpoint =
-// 	"http://obaliquid.staging.aquabrowser.nl/onderwijs/api/v1/search/?q=eigen%20onderneming+NOT+lom.lifecycle.contribute.publisher%3Dwikipedia&authorization=0076bc3bc11d080e07a303360178002a";
-const endpoint =
-	"https://zoeken.oba.nl/api/v1/search/?q=ondernemen&authorization=0076bc3bc11d080e07a303360178002a";
-fetch(endpoint, { mode: "no-cors" })
-	.then((res) => res.text())
-	.then((data) => console.log(data))
-	.catch((err) => console.log(err));
