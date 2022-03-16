6448fd51f91c5deba0c0b94eb305eeb3ba0f2380
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-16 15:35:25 +0100

    catch error in fetch

--- src/js/modules/fetch.js
+++ src/js/modules/fetch.js
@@ -1,11 +1,15 @@
-import { endpoint, key } from "./variables.js";
+// import { endpoint, key } from "./variables.js";
 
 const config = {
 	mode: "no-cors",
 };
 
-export default async function () {
-	const response = await fetch(endpoint);
-	const data = await response.json();
-	return data;
+export default async function (endpoint) {
+	try {
+		const response = await fetch(endpoint);
+		const data = await response.json();
+		return data;
+	} catch (err) {
+		console.log(err);
+	}
 }
