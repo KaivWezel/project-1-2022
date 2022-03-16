384b09a255c8369639a68c45f2512238f3d6d40e
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-16 15:35:54 +0100

    attempt on fetching data with api

--- src/js/modules/Router.js
+++ src/js/modules/Router.js
@@ -1,4 +1,5 @@
 import { loadBooks } from "./ui.js";
+import getData from "./fetch.js";
 
 export default async function handleRoutes() {
 	console.log("hashchange", window.location.hash);
@@ -6,7 +7,11 @@ export default async function handleRoutes() {
 		case "#stappenplan":
 			loadBooks();
 			break;
-
+		case "#zoeken":
+			const endpoint =
+				"http://obaliquid.staging.aquabrowser.nl/onderwijs/api/v1/search/?q=eigen%20onderneming+NOT+lom.lifecycle.contribute.publisher%3Dwikipedia&authorization=0076bc3bc11d080e07a303360178002a";
+			const data = getData(endpoint);
+			break;
 		default:
 			break;
 	}
