faf88e4ca23ef2f572603bc26e3b58a9cf581c62
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-17 10:15:51 +0100

    updated src for db

--- src/js/modules/ui.js
+++ src/js/modules/ui.js
@@ -1,11 +1,11 @@
 import getData from "./fetch.js";
 import { createCard } from "./components/Card.js";
 import Render from "./Renderer.js";
-import { localDb } from "./variables.js";
+import { booksUrl } from "./variables.js";
 
 export const loadBooks = async () => {
 	const bookshelf = document.querySelector(".bookshelf");
-	const books = await getData(localDb);
+	const books = await getData(booksUrl);
 	books.forEach((book) => {
 		const card = createCard(book);
 		Render(card, bookshelf);
--- src/js/modules/variables.js
+++ src/js/modules/variables.js
@@ -1,7 +1,7 @@
 // const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
 
-const endpoint = "http://localhost:3000/results";
+const booksUrl = "http://localhost:3000/books";
+const resultsUrl = "http://localhost:3000/results";
 const key = "0076bc3bc11d080e07a303360178002a";
 const secret = "6b02878111c06660300798cf4c40a685";
-const localDb = "http://localhost:3000/results";
-export { endpoint, key, secret, localDb };
+export { booksUrl, resultsUrl, key, secret };
