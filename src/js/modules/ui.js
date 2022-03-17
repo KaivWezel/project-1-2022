a431748b0a058310619c61a3bd2614cbc80b4c59
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-17 11:08:30 +0100

    Created resultsList

--- src/js/modules/ui.js
+++ src/js/modules/ui.js
@@ -1,13 +1,21 @@
 import getData from "./fetch.js";
-import { createCard } from "./components/Card.js";
+import { bookCard, resultCard } from "./components/Card.js";
 import Render from "./Renderer.js";
-import { booksUrl } from "./variables.js";
+import { booksUrl, resultsUrl } from "./variables.js";
 
 export const loadBooks = async () => {
 	const bookshelf = document.querySelector(".bookshelf");
 	const books = await getData(booksUrl);
 	books.forEach((book) => {
-		const card = createCard(book);
+		const card = bookCard(book);
 		Render(card, bookshelf);
+	});
+};
+
+export const searchResults = async () => {
+	const resultsList = document.querySelector(".searchResults");
+	const results = await getData(resultsUrl);
+	results.forEach((result) => {
+		const card = resultCard(result);
 	});
 };
