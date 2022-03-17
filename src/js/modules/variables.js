3c0e507148c2cd66a553e3531c38e53ba7354331
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-17 11:08:36 +0100

    Tried cors proxy

--- src/js/modules/variables.js
+++ src/js/modules/variables.js
@@ -1,7 +1,8 @@
-// const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
+const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
 
+const cors = "https://cors-anywhere.herokuapp.com/";
 const booksUrl = "http://localhost:3000/books";
 const resultsUrl = "http://localhost:3000/results";
 const key = "0076bc3bc11d080e07a303360178002a";
 const secret = "6b02878111c06660300798cf4c40a685";
-export { booksUrl, resultsUrl, key, secret };
+export { endpoint, cors, booksUrl, resultsUrl, key, secret };
