a470aa7b184c2d6214bc0443dfb936a397da582b
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-17 11:08:47 +0100

    Started #zoeken route

--- src/js/modules/Router.js
+++ src/js/modules/Router.js
@@ -1,5 +1,6 @@
-import { loadBooks } from "./ui.js";
+import { loadBooks, searchResults } from "./ui.js";
 import getData from "./fetch.js";
+import { endpoint, cors, resultsUrl } from "./variables.js";
 
 export default async function handleRoutes() {
 	console.log("hashchange", window.location.hash);
@@ -8,9 +9,7 @@ export default async function handleRoutes() {
 			loadBooks();
 			break;
 		case "#zoeken":
-			const endpoint =
-				"http://obaliquid.staging.aquabrowser.nl/onderwijs/api/v1/search/?q=eigen%20onderneming+NOT+lom.lifecycle.contribute.publisher%3Dwikipedia&authorization=0076bc3bc11d080e07a303360178002a";
-			const data = getData(endpoint);
+			searchResults();
 			break;
 		default:
 			break;
