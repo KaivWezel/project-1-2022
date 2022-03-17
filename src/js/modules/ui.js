c6ead1b45cd5936e13ea99bcdefe2a174b06eca6
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-17 20:15:54 +0100

    Added navigation

--- src/js/modules/ui.js
+++ src/js/modules/ui.js
@@ -1,7 +1,29 @@
 import getData from "./fetch.js";
 import { bookCard, resultCard } from "./components/Card.js";
 import Render from "./Renderer.js";
-import { booksUrl, resultsUrl } from "./variables.js";
+import { booksUrl, resultsUrl, endpoint } from "./variables.js";
+
+// TODO loader function
+
+export const activatePage = async () => {
+	const id = window.location.hash;
+	const sections = document.querySelectorAll("section");
+
+	const page =
+		window.location.hash === ""
+			? document.querySelector("#home")
+			: document.querySelector(id);
+
+	const _404 = document.querySelector("#notFound");
+	sections.forEach((section) => {
+		section.classList.remove("active");
+	});
+	if (page) {
+		page.classList.add("active");
+	} else {
+		_404.classList.add("active");
+	}
+};
 
 export const loadBooks = async () => {
 	const bookshelf = document.querySelector(".bookshelf");
@@ -17,5 +39,6 @@ export const searchResults = async () => {
 	const results = await getData(resultsUrl);
 	results.forEach((result) => {
 		const card = resultCard(result);
+		Render(card, resultsList);
 	});
 };
