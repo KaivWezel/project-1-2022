7fe97e7b3b941c7a89c4d7a705cc41af81e414e5
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-17 11:56:38 +0100

    Update variables.js

--- src/js/modules/variables.js
+++ src/js/modules/variables.js
@@ -1,8 +1,8 @@
-const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
+const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=eigen%20onderneming";
 
 const cors = "https://cors-anywhere.herokuapp.com/";
 const booksUrl = "http://localhost:3000/books";
-const resultsUrl = "http://localhost:3000/results";
+const resultsUrl = "http://localhost:3000/onderneming";
 const key = "0076bc3bc11d080e07a303360178002a";
 const secret = "6b02878111c06660300798cf4c40a685";
 export { endpoint, cors, booksUrl, resultsUrl, key, secret };
