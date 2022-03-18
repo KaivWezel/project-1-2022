120e6edbb6fb9a2e27a8a729c674abd7d2f9b43c
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-03-18 08:45:50 +0100

    Added DOM variables

--- src/js/modules/variables.js
+++ src/js/modules/variables.js
@@ -6,3 +6,10 @@ const resultsUrl = "http://localhost:3000/onderneming";
 const key = "0076bc3bc11d080e07a303360178002a";
 const secret = "6b02878111c06660300798cf4c40a685";
 export { endpoint, cors, booksUrl, resultsUrl, key, secret };
+
+// DOM elements
+const strategy = document.querySelector(".bookshelf");
+const taxes = document.querySelector(".taxes");
+const performance = document.querySelector(".performance");
+
+export { strategy, taxes, performance };
